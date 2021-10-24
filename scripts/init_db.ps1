0c484b7b0764ff6d43b4e5c915da4dd0c3c5278f
michaelpf97 <michaelpf97@gmail.com>
2021-10-24 21:13:12 +0100

    Improved init scripts

--- scripts/init_db.ps1
+++ scripts/init_db.ps1
@@ -1,15 +1,32 @@
 # Here we check for already existing credentials. Otherwise, set to default
 if ($env:DB_USER -eq $null) { $env:DB_USER="postgres" }
-if ($env:DB_PASSWORD -eq $null) { $env:DB_PASSWORD="password" }
+if ($env:PGPASSWORD -eq $null) { $env:PGPASSWORD="password" }
 if ($env:DB_NAME -eq $null) { $env:DB_NAME="newsletter" }
 if ($env:DB_PORT -eq $null) { $env:DB_PORT="5432" }
 
 docker run `
     -e POSTGRES_USER=${env:DB_USER} `
-    -e POSTGRES_PASSWORD=${env:DB_PASSWORD} `
+    -e POSTGRES_PASSWORD=${env:PGPASSWORD} `
     -e POSTGRES_DB=${env:DB_NAME} `
     -p ${env:DB_PORT}:5432 `
     -d `
     postgres
 
-$env:DATABASE_URL="postgres://${env:DB_USER}:${env:DB_PASSWORD}@localhost:${env:DB_PORT}/${env:DB_NAME}"
+$tries = 0
+while ($tries -lt 5) {
+    psql -h "localhost" -U "postgres" -p 5432 -d "newsletter" -c '\q' 2>&1
+    if ($LASTEXITCODE -ne 0) { 
+        $tries += 1 
+        Write-Output "Docker not responding..."
+        Write-Output "Trying again in 5 seconds!"
+        Start-Sleep -Seconds 5
+    }
+    else {
+        break
+    }
+}
+
+Write-Output "Postgres is up and running on port: $env:DB_PORT"  
+$env:DATABASE_URL="postgres://${env:DB_USER}:${env:PGPASSWORD}@localhost:${env:DB_PORT}/${env:DB_NAME}"
+sqlx database create
+Write-Output "Database '$env:DB_NAME' created!"  
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,4 +15,13 @@ docker run \
     -d postgres \
     postgres -N 1000
 
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
+echo "Database '${DB_NAME}' created!"
